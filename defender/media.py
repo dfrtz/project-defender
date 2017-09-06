c9591725a03ef781ea8cd7a8c768bbd9797a357e
David Fritz <dfritz@solaristudios.com>
2017-09-05 19:37:52 -0600

    Updated installation with ffmpeg compile

--- defender/media.py
+++ defender/media.py
@@ -29,8 +29,8 @@ class VideoStream(object):
         self._stop_event.clear()
 
         stream = cv2.VideoCapture(self.source)
-        # stream.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-        # stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+        # stream.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        # stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 960)
 
         while not stream.isOpened() and not self._stop_event.is_set():
             print('Video stream could not open, waiting {} seconds and trying again'.format(5))
