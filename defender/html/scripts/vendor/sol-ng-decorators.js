0c5b8e157c92d37fcb78258e48da84eced66dff9
David Fritz <dfrtzdev@gmail.com>
2018-01-13 13:41:51 -0700

    Updated Solari JS libraries to v1.0

--- defender/html/scripts/vendor/sol-ng-decorators.js
+++ defender/html/scripts/vendor/sol-ng-decorators.js
@@ -18,6 +18,32 @@ angular.module("solDecorator", ["ngMaterial", "ngAnimate", "ngRoute", "ngSanitiz
 
 /**
  * @ngdoc directive
+ * @name solJsonText
+ *
+ * @description
+ * Converts text in pretty printed json.
+ *
+ * @element ANY
+ */
+angular.module("solDecorator").directive("solJsonText", function() {
+    return {
+          restrict: "A",
+          require: "ngModel",
+          link: function(scope, element, attr, ngModel) {
+            function into(input) {
+              return JSON.parse(input);
+            }
+            function out(data) {
+              return JSON.stringify(data, undefined, 4);
+            }
+            ngModel.$parsers.push(into);
+            ngModel.$formatters.push(out);
+          }
+      };
+});
+
+/**
+ * @ngdoc directive
  * @name solLongClick
  *
  * @description
@@ -324,7 +350,7 @@ angular.module("solDecorator").directive("solFlipper", ["$window", function ($wi
 
     return {
         restrict: "E",
-        controller: function ($scope, $element, $attrs) {
+        controller: function ($scope) {
             var self = this;
 
             self.width = 0;
